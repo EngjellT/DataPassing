d7d59b2366fb896afb3523020c6c0c7e1921ccc2
Engjell <Engjot22@gmail.com>
2021-10-22 12:35:24 -0400

    Add project files.

---
+++ DataPassing/ChildWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataPassing
+{
+    /// <summary>
+    /// Interaction logic for ChildWindow.xaml
+    /// </summary>
+    public partial class ChildWindow : Window
+    {
+        public DataTransferDelegate dataTransferDelegate;
+
+        public ChildWindow(DataTransferDelegate del)
+        {
+            InitializeComponent();
+
+            dataTransferDelegate = del;
+            
+            
+
+            InitCombo();
+        }
+
+        private void InitCombo()
+        {
+         
+        }
+
+      
+        private void BTNsub_Click(object sender, RoutedEventArgs e)
+        {
+            dataTransferDelegate?.Invoke(txtFullN.Text.ToString());
+            
+        }
+    }
+}
---
+++ DataPassing/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataPassing
+{
+    public delegate void DataTransferDelegate(string data);
+
+    public partial class MainWindow : Window
+    {
+        public DataTransferDelegate del;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            del += new DataTransferDelegate(PassData);
+           
+        }
+
+        private void BtnOpenWindow_Click(object sender, RoutedEventArgs e)
+        {
+            ChildWindow childWindow = new ChildWindow(del);
+            childWindow.Show();
+        }
+
+        public void PassData(string data)
+        {
+            LblDisplay.Content = data;
+           
+        }
+    }
+}
